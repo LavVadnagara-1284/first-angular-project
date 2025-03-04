926ec8989ce35364b6db543813dbdc6ad7649cad
LavVadnagara1284 <lav.vadnagara@adit.com>
2025-03-04 18:40:08 +0530

    3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomeComponent},
+    { path:'profile', component: ProfileComponent},
+    { path:'about', component: AboutComponent},
+    { path:'contact', component: ContactComponent},
+    { path:'login', component: LoginComponent},
+    { path:'**', component: PageNotFoundComponent},
+];
