ea1d871223fdbcab96bfca25dd74272cafc9d3a4
LavVadnagara1284 <lav.vadnagara@adit.com>
2025-03-05 10:27:24 +0530

    4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
     { path:'', component: HomeComponent},
-    { path:'profile', component: ProfileComponent},
+    { path:'user/:id/:name', component: UserComponent},
     { path:'about', component: AboutComponent},
     { path:'contact', component: ContactComponent},
     { path:'login', component: LoginComponent},
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -8,10 +8,36 @@ import { Router, RouterLink } from '@angular/router';
     styleUrl: './home.component.css',
 })
 export class HomeComponent {
-    constructor(private router: Router) {}
-    goToProfile() {
-        this.router.navigate(['profile'], {
-            queryParams: { name: 'Lav Vadnagara' },
-        });
-    }
+    users = [
+        {
+            id: '1',
+            name: 'Lav Vadnagara',
+            age: 23,
+            email: 'Lav.Vadnagara@example.com',
+        },
+        {
+            id: '2',
+            name: 'Jhanvi Acharya',
+            age: 24,
+            email: 'Jhanvi.Acharya@example.com',
+        },
+        {
+            id: '3',
+            name: 'Purvraj Barot',
+            age: 23,
+            email: 'Purvraj.Barot@example.com',
+        },
+        {
+            id: '4',
+            name: 'Rudra Patel',
+            age: 21,
+            email: 'Rudra.Patel@example.com',
+        },
+        {
+            id: '5',
+            name: 'Yash Patel',
+            age: 22,
+            email: 'Yash.Patel@example.com',
+        },
+    ];
 }
